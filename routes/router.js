600a8429a7fc652d8f6871e4274001f14d594e49
subasshrestha513@gamil.com <subasshrestha>
2020-01-25 15:06:56 +0545

    find completed

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,21 @@
 const express = require('express');
 let router = express.Router();
+let Post = require('./../models/Post');
 
 //home route
 router.get('/',function(request,response){
-    response.render("index",{
-        title: "Home Page",
-        description:"Some decription"
+    Post.find({},function(err,posts){
+        if(err){
+            console.log(err);
+        }else{
+            response.render("index",{
+                title: "Home Page",
+                description:"Some decription",
+                posts : posts
+            });
+        }
     });
+    
 });
 
 //create post
@@ -14,10 +23,31 @@ router.get('/post/create',function(request,response){
     response.render("addPost");
 })
 
+//display each post
+router.get('/post/:id',function(request,response){
+    Post.findById(request.params['id'],function(err,post){
+        if(err){
+            console.log(err);
+        }else{
+            response.render('displayPost',{
+                post:post
+            });
+        }
+    });
+});
+
 //submit Post
 router.post('/submitPost',function(request,response){
-    console.log(request.body);
-    response.send('Post Successful');
+    let post= new Post(request.body);
+    post.save(function(err){
+        if(err){
+            console.log('cannot save post');
+            console.log(err);
+        }else{
+            console.log('Post saved successfully')
+            response.redirect('/')
+        }
+    });
 })
 
 //Home route
